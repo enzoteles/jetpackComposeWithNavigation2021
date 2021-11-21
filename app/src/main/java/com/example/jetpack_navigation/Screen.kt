b3358489d07481ee8e26dc5cb5eb896ff2f0afaf
Enzo <enzo.carvalho.teles@gmail.com>
2021-11-21 00:08:12 -0300

    Ajustando a navegaçao para passar o parametro index e recuperar o objeto via index.

--- app/src/main/java/com/example/jetpack_navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jetpack_navigation
-
-import androidx.annotation.StringRes
-
-sealed class Screen(val route:String, @StringRes val resourceId:Int){
-    object MainScreen: Screen(route = "main", resourceId = R.string.main_screen)
-    object DetailScreen: Screen(route = "detail", resourceId = R.string.detail_screen)
-}
